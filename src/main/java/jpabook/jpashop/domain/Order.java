3d595930e22e4fbe8486d1d1dd8b6b97896cec24
ohyuchan123 <oyuchan50@gmail.com>
2023-03-20 09:09:09 +0900

    주문,주문상품 Entity 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -48,4 +48,43 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member_DM member_dm,Delivery delivery,
+                                    OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member_dm);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem:orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능하다.");
+        }
+        this.setStatus(OrderStatus.CANSEL);
+        for(OrderItem orderItem : orderItems){
+             orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    public int getTotalPrice() {
+        int totalPrice = 0;
+
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
 }
